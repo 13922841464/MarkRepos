17d05b0001943414d3fbc2d5f99cc2229d4f4025
Mark <13922841464@126.com>
2015-03-09 16:45:20 +0800

    删除AdultVideo项目。

--- Source/MarkAndroid/AdultVideo/src/main/java/com/markzhai/adultvideo/AdultVideoApp.java
+++
@@ -1,9 +0,0 @@
-package com.markzhai.adultvideo;
-
-import com.markzhai.library.framework.BaseApplication;
-
-/**
- * Created by marktlzhai on 2015/2/28.
- */
-public class AdultVideoApp extends BaseApplication{
-}
